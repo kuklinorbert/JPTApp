6b73e31095575e5c1e3bd52c918dfe6b9cad8c81
kuklinorbert <kuklinorbert@gmail.com>
2021-04-15 11:34:30 +0200

    final fixed

--- lib/features/jptapp/presentation/pages/details_page.dart
+++ lib/features/jptapp/presentation/pages/details_page.dart
@@ -1,60 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:jptapp/features/jptapp/data/models/html_tag_model.dart';
 import 'package:jptapp/features/jptapp/data/models/item_model.dart';
+import 'package:jptapp/features/jptapp/data/models/pdf_link_model.dart';
 
 class DetailsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    ItemModel test = ModalRoute.of(context).settings.arguments;
+    ItemModel items = ModalRoute.of(context).settings.arguments;
+    List<PdfLinkModel> pdfs = items.pdfLinks;
+    List<HtmlTagModel> htmls = items.htmlTags;
     return Scaffold(
         appBar: AppBar(
-          title: Text(test.title),
+          title: Text(items.title),
           textTheme: Theme.of(context).textTheme,
         ),
         body: ListView(
           children: [
-            ListView.builder(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                itemCount: test.pdfLinks.length,
-                itemBuilder: (_, i) {
-                  return Column(
-                    children: [
-                      ListTile(
-                          title: Text(test.pdfLinks[i].title,
-                              style: Theme.of(context).textTheme.bodyText1),
-                          onTap: () {
-                            Navigator.of(context)
-                                .pushNamed('/pdf', arguments: test.pdfLinks[i]);
-                          },
-                          trailing: Icon(Icons.arrow_forward_ios,
-                              color: Theme.of(context).accentColor)),
-                      Divider()
-                    ],
-                  );
-                }),
-            ListView.builder(
-                shrinkWrap: true,
-                physics: ClampingScrollPhysics(),
-                itemCount: test.htmlTags.length,
-                itemBuilder: (_, i) {
-                  return Column(
-                    children: [
-                      ListTile(
-                        trailing: Icon(
-                          Icons.arrow_forward_ios,
-                          color: Theme.of(context).accentColor,
-                        ),
-                        title: Text(test.htmlTags[i].title,
-                            style: Theme.of(context).textTheme.bodyText1),
-                        onTap: () {
-                          Navigator.of(context).pushNamed('/html',
-                              arguments: test.htmlTags[i].html);
-                        },
-                      ),
-                      Divider()
-                    ],
-                  );
-                }),
+            pdfs == null
+                ? Container()
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemCount: pdfs.length,
+                    itemBuilder: (_, i) {
+                      return Column(
+                        children: [
+                          ListTile(
+                              title: Text(pdfs[i].title,
+                                  style: Theme.of(context).textTheme.bodyText1),
+                              onTap: () {
+                                Navigator.of(context)
+                                    .pushNamed('/pdf', arguments: pdfs[i]);
+                              },
+                              trailing: Icon(Icons.arrow_forward_ios,
+                                  color: Theme.of(context).accentColor)),
+                          Divider()
+                        ],
+                      );
+                    }),
+            htmls == null
+                ? Container()
+                : ListView.builder(
+                    shrinkWrap: true,
+                    physics: ClampingScrollPhysics(),
+                    itemCount: htmls.length == null ? 0 : htmls.length,
+                    itemBuilder: (_, i) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            trailing: Icon(
+                              Icons.arrow_forward_ios,
+                              color: Theme.of(context).accentColor,
+                            ),
+                            title: Text(htmls[i].title,
+                                style: Theme.of(context).textTheme.bodyText1),
+                            onTap: () {
+                              Navigator.of(context)
+                                  .pushNamed('/html', arguments: htmls[i].html);
+                            },
+                          ),
+                          Divider()
+                        ],
+                      );
+                    }),
           ],
         ));
   }
